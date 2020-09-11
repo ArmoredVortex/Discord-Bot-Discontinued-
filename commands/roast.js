c70adcf2144a971b78c95ce6454a7ab3912d2844
Rachit-Pandey <66690593+Rachit-Pandey@users.noreply.github.com>
2020-09-11 09:41:28 +0530

    Add files via upload

---
+++ commands/roast.js
@@ -0,0 +1,13 @@
+const roastMsg = ['If i were to die i would jump off thier ego to their IQ',`You know why mirrors can't laugh?`,`They were born in a highway, coz that is where the most accidents happen`,`When their mum dropped you them at school, she got fined for littering.`,`They have the right to remain silent because what they're gonna say next would probably be stupid anyways.`,`Have they been shopping lately? I've heard they're selling lives at the mall they should go get one.`];
+module.exports = {
+    name : 'roast {mention}',
+    description : 'Roasts the user you mention',
+    execute(message,args){
+        let taggedUser = message.mentions.users.first();
+        if(taggedUser === undefined){
+        message.channel.send('Enter a valid arg')
+        }else{
+        message.channel.send(`${roastMsg[Math.floor(Math.random()*roastMsg.length)]}`)
+        }
+    }
+}
