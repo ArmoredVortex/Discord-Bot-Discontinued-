db70ba1256b237481281188cdd5d70143b6dd9c6
Rachit-Pandey <66690593+Rachit-Pandey@users.noreply.github.com>
2020-09-17 17:01:11 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -6,29 +6,29 @@ const files = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 client.commands = new Discord.Collection();
 
 client.once('ready', () => {
-	console.log('Ready!');
+    for(const file of files){
+        const command = require(`./commands/${file}`)
+        client.commands.set(command.name,command);
+        console.log(`Loaded ${file}`)
+    }
+    console.log(`Logged in as ${client.user}`);
 });
 
 client.on('message', message => {
-    if (!message.content.startsWith(config.prefix) || message.author.bot) return;
+    if (!message.content.startsWith(config.prefix) || message.author.bot){
+    return;
+    }
 
 	const args = message.content.slice(config.prefix.length).trim().split(/ +/);
-    const command = args.shift().toLowerCase();
+    const commandName = args.shift().toLowerCase();
     
-    for(const file of files){
-        const command = require(`./commands/${file}`)
-        client.commands.set(command.name, command)
-    }
-
-	if (command === 'ping') {
-		client.commands.get('ping').execute(message,args);
-	} else if (command === 'generatenumber') {
-        client.commands.get('generatenumber {max}').execute(message,args);
-	} else if(command === 'help'){
-        client.commands.get('help').execute(message,args);
-    } else if (command === 'greet') {
-        client.commands.get('greet {mention}').execute(message,args);
-    }
+    if(!client.commands.has(commandName)) return;
+    const command = client.commands.get(commandName);
+    try{
+        command.execute(message,args);
+    } catch(error){
+        console.error(error)
+    };
 });
 
 client.login(config.token);
